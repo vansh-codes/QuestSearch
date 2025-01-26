9e759567f31f55cd72abe81a03a60efbcd0f00b0
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2025-01-27 04:06:00 +0530

    chore: update server configuration and routes, add health check endpoint, and modify Vercel settings

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,13 +12,16 @@ app.use(cors());
 app.use(express.json());
 
 //ROUTES:-
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 
 app.use('/api', searchRoutes);
 
 app.use(errorHandler);
 
-app.use('*', (req, res) => {
-    res.status(404).sendFile(__dirname + '/404.html');
+app.use((req, res) => {
+    res.status(404).sendFile('404.html', { root: __dirname });
 });
 
 export default app;
